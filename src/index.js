e7a45f1dc3a436bf06ebcd62026a3f517804f125
Jack Laird <jack.laird0@gmail.com>
2018-05-14 17:54:26 -0600

    Wrap app component with provider and set up store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Container/App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './Reducers/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && 
+                 window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
