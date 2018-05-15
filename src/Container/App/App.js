7a74c94e20b786ad7f9619ad619265d7a38c71bf
Tony Robertson <tonyr729@gmail.com>
2018-05-15 15:54:12 -0600

    Added temporary fetch method to display cards
    Added fetch method to componentDidMount.
    Currently storing movie data in Apps state but will replace with store functionality.

--- src/Container/App/App.js
+++ src/Container/App/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import {fetchMovieData} from '../../Helpers/apiCalls.js'
 import './App.css';
 
 class App extends Component {
+  constructor(){ 
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+
+  async componentDidMount(){
+    const movieData = await fetchMovieData()
+    const movieDisplay = movieData.map(movie => {
+      return (
+        <div>
+          <p>{movie.title}</p>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.image}`} alt=""/>
+        </div>
+
+      )
+    })
+    this.setState({
+      movies: movieDisplay
+    })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        REACT YO FACE!
+      <div>
+        { this.state.movies }
       </div>
     );
   }
---
+++ src/Reducers/reducer.js
@@ -0,0 +1,3 @@
+import { combineReducers } from "../../../../../../Library/Caches/typescript/2.8/node_modules/redux";
+
+export const rootReducer = combineReducers({});
