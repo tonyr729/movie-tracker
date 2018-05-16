e1a1bd638a5753f116ffc3f38f6de02c789633bd
Jack Laird <jack.laird0@gmail.com>
2018-05-16 17:11:32 -0600

    Add tests for movies reducer

---
+++ src/Reducers/moviesReducer.test.js
@@ -0,0 +1,36 @@
+import { moviesReducer } from './moviesReducer';
+
+describe('movies reducer', () => {
+  it('returns default state if the action type doesn\'t match', () => {
+    let initialState = [];
+    
+    expect(moviesReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('returns state with a new movie when called', () => {
+    let initialState = [];
+    let movieData = [{movie: 'test movie'}, {movie: 'anotha one'}];
+    let addMovieAction = {
+      type: 'ADD_MOVIES',
+      movieData 
+    };
+
+    let newState = moviesReducer(initialState, addMovieAction);
+
+    expect(newState).toEqual(movieData);
+  });
+
+  it('returns an updated state with the new movie when called', () => {
+    let currentState = [{movie: 'Avengers'}];
+    let movieData = [{movie: 'test movie'}, {movie: 'anotha one'}];
+    let addMovieAction = {
+      type: 'ADD_MOVIES',
+      movieData 
+    };
+    let expected = [{movie: 'Avengers'}, {movie: 'test movie'}, {movie: 'anotha one'}];
+
+    let newState = moviesReducer(currentState, addMovieAction);
+
+    expect(newState).toEqual(expected);
+  });
+});
