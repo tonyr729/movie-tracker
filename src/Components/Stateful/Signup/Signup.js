e06925a553a8a91e5556777d5eb503dc9b61ea1d
Tony Robertson <tonyr729@gmail.com>
2018-05-17 13:59:12 -0600

    Add user signup functionality
    Added logic to signup component with succesful post to backend.

--- src/Components/Stateful/Signup/Signup.js
+++ src/Components/Stateful/Signup/Signup.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
+import {signUp} from './../../../Helpers/apiCalls'
 
 class Signup extends Component {
   constructor() {
     super();
     this.state = {
+      name: '',
       email: '',
       password: ''
     };
   }
 
+  handleSignup = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = async (event) => {
+    const email = this.state.email.toLowerCase();
+    event.preventDefault();
+    const message = await signUp(this.state.name, this.state.email, this.state.password)
+    console.log(message)
+  }
+
+
   render() {
     return(
-      <form action="">
-        <input type="text" placeholder='Email'/>
-        <input type="text" placeholder='Password'/>
-        <input type="text" placeholder='Confirm Password'/>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder='Name' name='name' value={this.state.name} onChange={this.handleSignup}/>
+        <input type="text" placeholder='Email' name='email' value={this.state.email} onChange={this.handleSignup}/>
+        <input type="text" placeholder='Password' name='password' value={this.state.password} onChange={this.handleSignup}/>
         <button>Submit</button>
       </form>
     );
