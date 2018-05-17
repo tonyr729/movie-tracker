f90a1b58f22f76f0530421a5f967975d2cf46c69
Tony Robertson <tonyr729@gmail.com>
2018-05-17 14:22:58 -0600

    Add action and reducer for login and logout

--- src/Actions/actions.js
+++ src/Actions/actions.js
@@ -7,3 +7,12 @@ export const addFavorites = (movie) => ({
   type: 'ADD_FAVORITE',
   movie
 });
+
+export const login = (user) => ({
+  type: 'LOGIN',
+  user
+})
+
+export const logout = () => ({
+  type: 'LOGOUT'
+})
