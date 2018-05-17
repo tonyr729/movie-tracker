88afb7ab300be40845d4f4a1cc69b704d5499378
sospinar21 <sospinar21@gmail.com>
2018-05-16 21:49:54 -0600

    Add style to buttons

--- src/Components/Stateless/NavBar/NavBar.js
+++ src/Components/Stateless/NavBar/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
     <header>
-      <NavLink to='/' className='nav'>Home</NavLink>;
-      <NavLink to='/login' className='nav'>Log In</NavLink>;
-      <NavLink to='/signup' className='nav'>Sign Up</NavLink>;       
-      <NavLink to='/favorites' className='nav'>Favorites</NavLink>;         
+      <NavLink to='/' className='nav'>Home</NavLink>
+      <NavLink to='/login' className='nav'>Log In</NavLink>
+      <NavLink to='/signup' className='nav'>Sign Up</NavLink>       
+      <NavLink to='/favorites' className='nav'>Favorites</NavLink>         
     </header>
   )
 }
