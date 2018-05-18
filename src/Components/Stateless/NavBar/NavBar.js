2da26b757f9cf0a67854faf275bc87ddf44178f3
sospinar21 <sospinar21@gmail.com>
2018-05-17 19:02:37 -0600

    Add sign out

--- src/Components/Stateless/NavBar/NavBar.js
+++ src/Components/Stateless/NavBar/NavBar.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
+import { connect } from 'react-redux';
 
-const NavBar = () => {
-  return (
-    <header>
-      <NavLink exact to='/' className='nav' activeClassName='selected'>Home</NavLink>
-      <NavLink exact to='/login' className='nav' activeClassName='selected'>Log In</NavLink>
-      <NavLink exact to='/signup' className='nav' activeClassName='selected'>Sign Up</NavLink>       
-      <NavLink exact to='/favorites' className='nav' activeClassName='selected'>Favorites</NavLink>         
-    </header>
-  )
+const NavBar = (props) => {
+  console.log(props.user)
+  if (!props.user.name) {
+    return (
+      <header>
+        <NavLink exact to='/' className='nav' activeClassName='selected'>Home</NavLink>      
+        <NavLink exact to='/login' className='nav' activeClassName='selected'>Log In</NavLink>
+        <NavLink exact to='/signup' className='nav' activeClassName='selected'>Sign Up</NavLink>  
+        <NavLink exact to='/favorites' className='nav' activeClassName='selected'>Favorites</NavLink>
+      </header>                       
+    )   
+  } else {
+    return (
+      <header>
+        <NavLink exact to='/' className='nav' activeClassName='selected'>Home</NavLink>
+        <button className='nav'>Sign Out</button>
+        <NavLink exact to='/favorites' className='nav' activeClassName='selected'>Favorites</NavLink>         
+      </header>
+    )
+  }
 }
 
-export default NavBar;
+const mapStateToProps = (state) => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(NavBar);
